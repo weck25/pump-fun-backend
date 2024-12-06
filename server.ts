deac1207ec617c1da54aa5e71630545c28778c8d
GodHad <sunharius@gmail.com>
2024-12-06 00:54:53 -0800

    update port setting

--- server.ts
+++ server.ts
@@ -3,15 +3,15 @@ import app from './src/app'
 import socketio from './src/sockets/'
 import { logger } from './src/sockets/logger';
 import { init } from './src/db/dbConncetion';
-
+const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   await init()
   const server = createServer(app);
   socketio(server);
 
-  server.listen(app.get('port'), () => {
-    logger.info('  App is running at http://localhost:%d in %s mode', app.get('port'), app.get('env'));
+  server.listen(PORT, () => {
+    logger.info('App is running at http://localhost:%d in %s mode', PORT, app.get('env'));
   });
   server.on('close', (error: any) => {
     logger.error('Server closed unexpectedly by the reason: ', error);
