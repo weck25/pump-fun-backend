37585b0816d869490a20176ac83e902c63286a01
GodHad <sunharius@gmail.com>
2025-01-10 10:27:01 -0800

    add my wallet to admin

--- src/db/dbConnection.ts
+++ src/db/dbConnection.ts
@@ -16,8 +16,7 @@ export const init = () => {
       if (!adminData) {
         const newAdminData = new AdminData({
           admin: [
-            '0x1066f339C393Cd41D1acF0f0AAE7CDE9f3B30596', 
-            '0x4191965460D99eA9486519727a91Dbf112bd4d5f',
+            '0xD6437Dc6Cc7369E9Fd7444d1618E21fffAD51A75',
             '0xb5C64BfD79f0048EA88E1699834273704aBAB3D3',
             '0xd1DD7014C690374e113AF710886097e6B68CBCdF'
           ],
